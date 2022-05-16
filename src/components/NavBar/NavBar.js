d518ef5cc3dbec2037fe872f36e1f6c64c1114d2
Alvaro Fernandez <afernandez@itba.edu.ar>
2022-05-16 12:07:13 -0300

    Primer commit del proyecto.

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Brauerei</h1>
+            <div>
+                <button>Nacionales</button>
+                <button>Importadas</button>
+                <button>Accesorios</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
